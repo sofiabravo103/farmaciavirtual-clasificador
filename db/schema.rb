07c5fcabf171fe6f59d313ed642c25a32ab0fa0a
Sofia Bravo <sofiabravo103@gmail.com>
2016-07-05 20:35:48 -0400

    Added psql database.yml

--- db/schema.rb
+++ db/schema.rb
@@ -13,13 +13,16 @@
 
 ActiveRecord::Schema.define(version: 20160705230830) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "datasets", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
   end
 
-  add_index "datasets", ["user_id"], name: "index_datasets_on_user_id"
+  add_index "datasets", ["user_id"], name: "index_datasets_on_user_id", using: :btree
 
   create_table "roles", force: :cascade do |t|
     t.string   "name"
@@ -29,8 +32,8 @@ ActiveRecord::Schema.define(version: 20160705230830) do
     t.datetime "updated_at"
   end
 
-  add_index "roles", ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
-  add_index "roles", ["name"], name: "index_roles_on_name"
+  add_index "roles", ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id", using: :btree
+  add_index "roles", ["name"], name: "index_roles_on_name", using: :btree
 
   create_table "tweets", force: :cascade do |t|
     t.text     "text"
@@ -40,7 +43,7 @@ ActiveRecord::Schema.define(version: 20160705230830) do
     t.integer  "dataset_id"
   end
 
-  add_index "tweets", ["dataset_id"], name: "index_tweets_on_dataset_id"
+  add_index "tweets", ["dataset_id"], name: "index_tweets_on_dataset_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
@@ -58,14 +61,16 @@ ActiveRecord::Schema.define(version: 20160705230830) do
     t.boolean  "approved",               default: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "users_roles", id: false, force: :cascade do |t|
     t.integer "user_id"
     t.integer "role_id"
   end
 
-  add_index "users_roles", ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id"
+  add_index "users_roles", ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id", using: :btree
 
+  add_foreign_key "datasets", "users"
+  add_foreign_key "tweets", "datasets"
 end
