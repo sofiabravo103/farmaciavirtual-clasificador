45526022458efe68587ede1967e010c4d530d956
Sofia Bravo <sofiabravo103@gmail.com>
2016-07-07 13:47:59 -0400

    Removed mailer action

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -2,7 +2,7 @@ class AdminMailer < ApplicationMailer
   def new_user_waiting_for_approval(user)
     @user = user
     unless User.with_role(:admin) == []
-      mail(to: User.with_role(:admin).first.email, subject: 'New user waiting for approval')
+      #mail(to: User.with_role(:admin).first.email, subject: 'New user waiting for approval')
     end
   end
 end
