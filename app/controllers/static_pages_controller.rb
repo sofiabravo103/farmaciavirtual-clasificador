ce126372b68653d7c1d4e6624ae30eb6cd540a67
Sofia Bravo <sofiabravo103@gmail.com>
2016-07-07 00:44:26 -0400

    Removed make admin from landing page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,9 +4,9 @@ class StaticPagesController < ApplicationController
   def landing
     if current_user and current_user.has_role? :admin
       @users_awaiting_approval = User.where(approved: false)
-#      @users_awaiting_approval = nil if @users_awaiting_approval == []
-      @approved_users = User.where(approved: true) - User.with_role(:admin)
-#      @approved_users = nil if @approved_users == []
+      @users_awaiting_approval = nil if @users_awaiting_approval == []
+      #@approved_users = User.where(approved: true) - User.with_role(:admin)
+      #@approved_users = nil if @approved_users == []
     end
   end
 end
