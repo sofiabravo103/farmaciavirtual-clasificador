f5702e2535f4ce6d4fcbde9293b24abf05215d42
Sofia Bravo <sofiabravo103@gmail.com>
2016-07-07 00:18:58 -0400

    Added permission

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,8 @@ module ClasificadorMeds
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    # Do not swallow errors in after_commit/after_rollback callbacks.
+    # Do not swallow errors in after_commit/after_rollback callback
+    config.web_console.development_only = false
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
